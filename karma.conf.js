577960be62a013701f5d9c339d0eb519b25584da
qory user <qory user>
2017-01-02 00:44:44 -0600

    Add comments to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,21 @@
+// specifies how we want to run our tests
 var webpackConfig = require('./webpack.config.js');
 module.exports = function (config){
   config.set({
-    browsers:['Chrome'],
+    browsers:['Chrome'], // browsers we want to use
     songleRun: true,
-    frameworks: ['mocha'],
-    files: ['app/tests/**/*.test.jsx'],
+    frameworks: ['mocha'], // framework we want to use
+    files: ['app/tests/**/*.test.jsx'], /// which test files to run
     preprocessors: {
-      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap'] // which preprocessors to use
     },
-    reporters: ['mocha'],
-    client:{
+    reporters: ['mocha'], // what reporters we want to use (cleanup the output/ test results)
+    client:{ // send details down to the individual frameworks
       mocha: {
           timeout:'5000' /// times out if test is not complete in 5 seconds
       }
     },
-    webpack: webpackConfig,
+    webpack: webpackConfig, // use the webpack information in webpack.config.js
     webpackServer: {
       noInfo: true
     }
