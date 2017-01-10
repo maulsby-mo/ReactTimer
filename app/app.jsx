6ef1a518510a5432688492b49e6c79530965f85f
qory user <qory user>
2017-01-10 00:46:06 -0600

    Update status to stop when count reaches zero

--- app/app.jsx
+++ app/app.jsx
@@ -17,7 +17,7 @@ ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
     <Route path="countdown" component={Countdown} />
-    <IndexRoute path="timer" component={Timer} ></IndexRoute>
+    <IndexRoute component={Timer} ></IndexRoute>
   </Route>
   </Router>,
   document.getElementById('app')
